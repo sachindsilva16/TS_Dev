5abae34be785f460f72f0bed6cc593a4d05666ab
sachindsilva16 <sachindsilva16@gmail.com>
2023-09-03 14:55:59 +0530

    add source map flag

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 "use strict";
+// let b = 10;
+// function onAdd(n1:number,n2:number){
+//     let a = 10;
+// }
+// function compare(n1:number,n2:number){
+//     if(n1 > n2){
+//         return n1;
+//     } else {
+//         return n2;
+//     }
+// }
+let a = 10;
+let b = 20;
+let c = a + b;
+``;
+for (let i = 0; i < 5; ++i) {
+    console.log(i);
+}
+console.log(c);
 console.log("Welcome to TypeScript - Strictly on data type - Updated");
+//# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,1 +1,39 @@
+
+
+
+// let b = 10;
+
+// function onAdd(n1:number,n2:number){
+//     let a = 10;
+
+// }
+
+// function compare(n1:number,n2:number){
+
+//     if(n1 > n2){
+//         return n1;
+//     } else {
+//         return n2;
+//     }
+// }
+
+
+
+let a = 10;
+let b = 20;
+
+let c = a + b;``
+
+for (let i = 0; i < 5;++i) {
+    console.log(i);
+}
+
+
+console.log(c);
+
+
+
 console.log("Welcome to TypeScript - Strictly on data type - Updated");
+
+
+
